9f03bb16ac5ded13e4f5573cd635887247ac8585
Deependra <70470040+deepak685@users.noreply.github.com>
2022-04-17 22:23:46 +0530

    Update validator.js

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,23 +1,50 @@
-const mongoose=require('mongoose')
+const mongoose=require("mongoose")
+// const { isValid } = require("shortid")
+ 
 
-const isValid=val=>{
-    if(typeof val===undefined || typeof val===null) return false
-    if(typeof val==="string" && val.trim().length<0) return false
+
+
+let isValid=function(value){
+    if(typeof value==="undefined" || typeof value===null) return false
+    if(typeof value==="string" && value.trim().length===0) return false
     return true
 }
 
-const isValidRequestBody = requestBody=> {
-    return Object.keys(requestBody).length > 0; // it checks, is there any key is available or not in request body
-};
-
-const isValidObjectId=objectId => mongoose.Types.ObjectId.isValid(objectId)
-
-const validString = value=> {
-    if (typeof value === 'string' && value.trim().length === 0) return false //it checks whether the string contain only space or not 
-    return true;
+let isValidrequestBody = function (requestBody) {
+    return Object.keys(requestBody).length !== 0
+}
+let isValidObjectId=function(objectId){
+    return mongoose.Types.ObjectId.isValid(objectId)
 }
 
-module.exports={isValid,isValidObjectId,isValidRequestBody,validString}
+const validstring=function(value){
+    if(typeof value==="string" && value.trim().length===0) return false
+    return true
+}
 
 
 
+const validstatus=function(status){
+    return["pending","completed","cancelled"].indexOf(status)!==-1
+}
+const isvalidInteger=function isInteger(value){
+    return value % 1==0
+}
+
+
+
+const validInstallment = value=> {
+    if (value < 0) return false
+    if (value % 1 == 0) return true;
+  }
+
+module.exports={
+     isValid,
+    isvalidInteger,
+    validstatus,
+    validstring,
+    isValidObjectId,
+     isValidrequestBody,
+     validInstallment
+
+}
